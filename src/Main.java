5bd5f6b6111fd02fb8368b69f4518eec89012bca
leily <leily@example.com>
2023-09-12 00:14:43 +0300

    Removed two lines into one

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int amount;
-        amount = 13676;
+        int amount = 13676;
         int rub = 20;
         System.out.println(amount / 20);
     }
